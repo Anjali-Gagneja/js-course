1491551856d0700634d4f5e3c8ace96cf59f61cf
Anjali Gagneja <shellyg3698@gmail.com>
2024-09-23 13:29:23 +0000

    datatypes

---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3);// we are using nodejs, not browser
+
+console.log(3 + 3);
+// code readability should be high
+console.log("Anjali");
+
+let name = "Anjali";
+let age = 18;
+let isLoggedIn = true;
+let state;
+
+//number => 2 to power 53
+//bigInt => used for big numbers...mostly used in big websites like stock market, reddit etc.
+// string => ""
+// boolean => true/false
+//null => standalone value
+//undefined => value not assigned yet
+//symbol => unique 
+
+
+// object
+
+console.log(typeof "Anjali");
+console.log(typeof undefined);
+console.log(typeof null);

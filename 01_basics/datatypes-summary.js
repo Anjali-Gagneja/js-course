ee2af7465e90e84ddc6a9fde7d210fbd21f6460c
Anjali Gagneja <shellyg3698@gmail.com>
2024-09-26 09:53:50 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -30,4 +30,35 @@ const myFunction = function() {
     //console.log("Hello World!");
 }
 
-console.log(typeof myFunction);
+//console.log(typeof myFunction);
+
+
+
+// **********************************
+
+// Stack(Premitive), heap(non-premitive)
+
+let myYoutubeName = "Anjali";
+
+
+let anotherName = myYoutubeName;
+anotherName = "Gagneja";
+console.log(anotherName);
+console.log(myYoutubeName);
+
+let userOne = {
+    email: "123@gmail.com",
+    upi: "user@yl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "anjali@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+// in stack memory if we make changes we are just making a copy of it and changes the copied value
+
+// while in heap memory that is used for non-premitive datatypes we just take the reference of the memory i.e., we actually hange the original value n the heap memory not the copied one because we are just taking reference.
